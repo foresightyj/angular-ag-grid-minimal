5c8cd291e7e0cdecf2bf06bacecedb8806e7097d
JasmineZjn <7780574+JasmineZjn@user.noreply.gitee.com>
2022-02-20 00:14:15 +0800

    hello-infite-row works with loading all data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
+import "@angular/compiler";
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { AgGridModule } from 'ag-grid-angular';
 import { HelloInfiniteRowComponent } from './hello-infinite-row/hello-infinite-row.component';
 import { HomeComponent } from './home/home.component';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +17,9 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    AgGridModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/hello-infinite-row/hello-infinite-row.component.ts
+++ src/app/hello-infinite-row/hello-infinite-row.component.ts
@@ -1,4 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import {
+  ColDef,
+  GridReadyEvent,
+  ICellRendererParams,
+  IDatasource,
+  IGetRowsParams,
+} from 'ag-grid-community';
+
+import { nameof } from "ts-simple-nameof";
+import { IUser } from "../../../types";
 
 @Component({
   selector: 'app-hello-infinite-row',
@@ -7,9 +18,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HelloInfiniteRowComponent implements OnInit {
 
-  constructor() { }
+  public columnDefs: ColDef[] = [
+    {
+      headerName: 'Id',
+      field: nameof<IUser>(m => m.id),
+    },
+    {
+      headerName: 'Name',
+      maxWidth: 100,
+      field: nameof<IUser>(m => m.name),
+    },
+    {
+      headerName: 'Gender',
+      field: nameof<IUser>(m => m.gender),
+    },
+  ];
+  public defaultColDef: ColDef = {
+    flex: 1,
+    resizable: true,
+    minWidth: 100,
+  };
+  public rowBuffer = 0;
+  public rowSelection = 'multiple';
+  public rowModelType = 'infinite';
+  public cacheBlockSize = 100;
+  public cacheOverflowSize = 2;
+  public maxConcurrentDatasourceRequests = 1;
+  public infiniteInitialRowCount = 1000;
+  public maxBlocksInCache = 10;
+  public rowData!: any[];
 
+  constructor(private http: HttpClient) { }
   ngOnInit(): void {
   }
 
+  onGridReady(params: GridReadyEvent) {
+    this.http
+      .get<any[]>('http://localhost:3000/users')
+      .subscribe((data) => {
+        const dataSource: IDatasource = {
+          rowCount: undefined,
+          getRows: function (params: IGetRowsParams) {
+            console.log(
+              'asking for ' + params.startRow + ' to ' + params.endRow
+            );
+            // At this point in your code, you would call the server.
+            // To make the demo look real, wait for 500ms before returning
+            setTimeout(function () {
+              // take a slice of the total rows
+              const rowsThisPage = data.slice(params.startRow, params.endRow);
+              // if on or after the last page, work out the last row.
+              let lastRow = -1;
+              if (data.length <= params.endRow) {
+                lastRow = data.length;
+              }
+              // call the success callback
+              params.successCallback(rowsThisPage, lastRow);
+            }, 500);
+          },
+        };
+        params.api!.setDatasource(dataSource);
+      });
+  }
 }
